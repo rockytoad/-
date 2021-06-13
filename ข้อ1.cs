f74731fe4d6058abe2117f28a15bd29073aa8284
roc <mario12577@hotmail.com>
2021-06-13 16:17:40 +0700

    no message

---
+++ ข้อ1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp34
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+           
+            char[,] number = new char[3, 3];
+            for (int j = 0; j < 3; j++)
+            {
+                for(int i = 0; i < 3; i++)
+                {
+                    number[i, j] = char.Parse(Console.ReadLine());
+                }
+            }
+           for(int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(number[j, i]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+        }
+    }
+    
+}
+            
+            
+
+
+
+
